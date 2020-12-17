c8ab39349080b34285ba50b6bff6000ca52d683d
Evan Leppink <leppink@mit.edu>
2020-12-16 22:33:07 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/reflectometryNN.jl
@@ -0,0 +1,5 @@
+module reflectometryNN
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using reflectometryNN
+using Test
+
+@testset "reflectometryNN.jl" begin
+    # Write your tests here.
+end
